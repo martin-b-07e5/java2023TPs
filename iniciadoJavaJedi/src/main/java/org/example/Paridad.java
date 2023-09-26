f6c1edd951fe558177ab9bf2364e73d01b26a00c
martin-b-07e5 <martin.bergagno@gmail.com>
2023-09-25 23:04:34 -0300

    iniciadoJavaJedi

---
+++ iniciadoJavaJedi/src/main/java/org/example/Paridad.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Paridad {
+  public static String paridad(int num1, int num2) {
+    int suma = num1 + num2;
+    return (suma % 2 == 0) ? "par" : "impar";
+  }
+}
---
+++ iniciadoJavaJedi/src/main/java/org/example/Suma.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Suma {
+  public static int suma(int num1, int num2) {
+    return (num1 + num2);
+  }
+}
