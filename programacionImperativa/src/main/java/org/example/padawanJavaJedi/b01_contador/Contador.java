bc43fc8535eb28b13caf0d667612e0f5cc33b34b
martin-b-07e5 <martin.bergagno@gmail.com>
2023-10-02 17:29:48 -0300

    padawanJavaJedi branch. Contador done.

---
+++ programacionImperativa/src/main/java/org/example/padawanJavaJedi/b01_contador/Contador.java
@@ -0,0 +1,12 @@
+package org.example.padawanJavaJedi.b01_contador;
+
+/* 1.Contador: guardar un número en un espacio de memoria e
+incrementar dicho número 20 veces sin usar asignación.
+¿Que operador podría usar para hacerlo? */
+public class Contador {
+  public static void main(String[] args) {
+    int num1 =1;
+    num1 = num1 + 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1+ 1;
+    System.out.println(num1);
+  }
+}
