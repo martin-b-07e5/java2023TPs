f82a29daf94ec0a9cc17818f2cf4dd6f0335592a
martin-b-07e5 <martin.bergagno@gmail.com>
2023-10-30 15:30:14 -0300

    FreeFall-FinalVelocity

--- programacionImperativa/src/main/java/org/example/dMaestroJavaJedi/d05KineticEnergy/KineticEnergy.java
+++ programacionImperativa/src/main/java/org/example/dMaestroJavaJedi/d05KineticEnergy/KineticEnergy.java
@@ -13,21 +13,21 @@ import java.util.Scanner;
 public class KineticEnergy {
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    double mass, velocity;
+    try (Scanner scanner = new Scanner(System.in)) {
 
-    System.out.println("Enter the mass of the object (in kg): ");
-    double mass = scanner.nextDouble();
+      System.out.println("Enter the mass of the object (in kg): ");
+      mass = scanner.nextDouble();
 
-    System.out.println("Enter the velocity of the object (in m/s)");
-    double velocity = scanner.nextDouble();
+      System.out.println("Enter the velocity of the object (in m/s)");
+      velocity = scanner.nextDouble();
+    }
 
     // Calculate the kinetic energy using the formula
     double kineticEnergy = 0.5 * mass * velocity * velocity;
 
     // Display the result
     System.out.println("The kinetic energy of the object is: " + String.format("%.0f", kineticEnergy) + " joules");
-
-
   }
 
 }
---
+++ programacionImperativa/src/main/java/org/example/dMaestroJavaJedi/d06FinalVelocity/FreeFall.java
@@ -0,0 +1,40 @@
+package org.example.dMaestroJavaJedi.d06FinalVelocity;
+
+/* Calculate the final velocity of an object in free fall,
+ given its initial velocity, the acceleration of gravity and the elapsed time.
+Use the formula: v_f = v_i + g * t
+Where
+ v_f is the final velocity,
+ v_i is the initial velocity,
+ g is the acceleration of gravity and
+ t is the elapsed time. */
+
+import java.util.Scanner;
+
+public class FreeFall {
+
+  public static void main(String[] args) {
+
+    double inicialVelocity, elapsedTime, finalVelocity;
+    // Constante para la aceleración debido a la gravedad (m/s^2)
+    final double GRAVITY = 9.81;
+
+    try (Scanner scanner = new Scanner(System.in)) {
+
+      System.out.println("Enter the initial velocity (in meters per second): ");
+      inicialVelocity = scanner.nextDouble();
+
+      System.out.println("Enter the elapsed time (in seconds): ");
+      elapsedTime = scanner.nextDouble();
+
+      // Calculate the final velocity using the formula
+      finalVelocity = inicialVelocity + GRAVITY * elapsedTime;
+
+      // Display the result
+      System.out.println("The final velocity of the object in free fall is: " + finalVelocity + " meters per second");
+    } catch (java.util.InputMismatchException e) {
+      System.err.println("Input error: Please enter valid numeric values.");
+    }
+  }
+
+}
