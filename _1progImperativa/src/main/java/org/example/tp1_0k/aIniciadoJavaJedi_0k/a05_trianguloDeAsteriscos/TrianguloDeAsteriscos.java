283ab48b73b84940ee427f80b4d9ae12d5d28841
martin-b-07e5 <martin.bergagno@gmail.com>
2023-11-01 02:32:59 -0300

    TriangleOfAsterisks done

--- _1progImperativa/src/main/java/org/example/tp1_0k/aIniciadoJavaJedi_0k/a05_trianguloDeAsteriscos/TrianguloDeAsteriscos.java
+++ _1progImperativa/src/main/java/org/example/tp1_0k/aIniciadoJavaJedi_0k/a05_trianguloDeAsteriscos/TrianguloDeAsteriscos.java
@@ -3,10 +3,10 @@ package org.example.tp1_0k.aIniciadoJavaJedi_0k.a05_trianguloDeAsteriscos;
 /*5. Triangulo de asteriscos: Imprimir un triángulo de asteriscos en la consola.*/
 public class TrianguloDeAsteriscos {
   public static void main(String[] args) {
-    System.out.println("    *");
-    System.out.println("   ***");
-    System.out.println("  *****");
-    System.out.println(" *******");
+    System.out.println("····*");
+    System.out.println("···***");
+    System.out.println("··*****");
+    System.out.println("·*******");
     System.out.println("*********");
   }
 }
