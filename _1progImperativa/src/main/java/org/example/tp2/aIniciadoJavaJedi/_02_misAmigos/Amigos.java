9bb3eed3cd0925d97bfe7cc352b843b87e48d51d
martin-b-07e5 <martin.bergagno@gmail.com>
2023-11-01 02:34:43 -0300

    Friends

---
+++ _1progImperativa/src/main/java/org/example/tp2/aIniciadoJavaJedi/_02_misAmigos/Amigos.java
@@ -0,0 +1,7 @@
+package org.example.tp2.aIniciadoJavaJedi._02_misAmigos;
+
+/* Save the name of N number of friends in variables, iterate over
+that amount showing the name of each friend. */
+
+public class Amigos {
+}
